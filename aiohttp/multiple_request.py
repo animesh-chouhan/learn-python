bc1beda56dc2d350e4a0f23d5e8227dccc8b1df9
Animesh Singh Chouhan <animeshsingh.iitkgp@gmail.com>
2021-07-10 16:34:58 +0530

    learnt aiohttp

---
+++ aiohttp/multiple_request.py
@@ -0,0 +1,18 @@
+# https://www.twilio.com/blog/asynchronous-http-requests-in-python-with-aiohttp\
+import aiohttp
+import asyncio
+import time
+
+start_time = time.time()
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        for number in range(1, 151):
+            pokemon_url = f'https://pokeapi.co/api/v2/pokemon/{number}'
+            async with session.get(pokemon_url) as resp:
+                pokemon = await resp.json()
+                print(pokemon['name'])
+
+asyncio.run(main())
+print("--- %s seconds ---" % (time.time() - start_time))
--- asyncio-threading-multiprocessing/requests/request_thread.py
+++ asyncio-threading-multiprocessing/requests/request_thread.py
@@ -5,17 +5,21 @@ from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 N = 100
 URL = "http://httpbin.org/uuid"
 
+
 def get_uuid(s, i):
     response = s.get(URL)
     print(str(i) + ": " + response.json()["uuid"] + "\n", end="")
 
 # https://docs.python.org/3/library/concurrent.futures.html#module-concurrent.futures
+
+
 def main():
-    with ThreadPoolExecutor() as executor:
-    # with ProcessPoolExecutor() as executor:
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        # with ProcessPoolExecutor(max_workers=10) as executor:
         with requests.Session() as s:
             for i in range(N):
                 executor.submit(get_uuid, s, i)
+
 
 if __name__ == "__main__":
     start_time = time.time()
